274c8e7f067ca8d89ff3a7cb3ce6a38c72f6742b
CYY <35159700+cyysc1998@users.noreply.github.com>
2022-07-25 19:51:21 +0800

    Create 155.cpp

---
+++ c++/155.cpp
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=155 lang=cpp
+ *
+ * [155] 最小栈
+ */
+
+// @lc code=start
+class MinStack {
+public:
+    std::stack<int> stack1, stack2;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        stack1.push(val);
+        if (stack2.size() == 0)
+            stack2.push(val);
+        else
+            stack2.push(min(val, stack2.top()));
+
+    }
+    
+    void pop() {
+        stack1.pop();
+        stack2.pop();
+    }
+    
+    int top() {
+        return stack1.top();
+    }
+    
+    int getMin() {
+        return stack2.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+// @lc code=end
+
